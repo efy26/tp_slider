f4a1e8b60017564ffd24fa9eec9ef6942f63f6f5
efy26 <eliezerefoyaefy2018@gmail.com>
2023-11-29 18:00:41 +0100

    ajout btn prev et next

--- js/script.js
+++ js/script.js
@@ -1,50 +1,90 @@
 let img = document.querySelectorAll(".img_slide img")
-let indice = 0
+const btns = document.querySelectorAll(".ensembe_btn input")
+
+let indice = 1
 
 
 
 const slideAu = (valeur) => {
     valeur.classList.add("active")
+
 } 
 
 const slideDi = (valeur) => {
     valeur.classList.remove("active")
+
+
 } 
 
 window.onload = () => {
     
     slideAu(img[indice])
-    imgSlide()
+
+    // btn prev
+    btns[0].addEventListener("click", imgSlidePrev)
+
+    btns[0].addEventListener("mouseover", imgSlideStop)
+    btns[0].addEventListener("mouseout", imgSlideStart)
+
+    // btn next
+    btns[1].addEventListener("click", imgSlide)
+
+    btns[1].addEventListener("mouseover", imgSlideStop)
+    btns[1].addEventListener("mouseout", imgSlideStart)
+        
+    
 }
 
+const imgSlidePrev = () => {
+
+
+    slideDi(img[indice])
+    slideAu(img[indice-1])
+
+    indice = indice - 1
+
+            
+    if (indice === 0 ) {
+        indice = 6
+        slideDi(img[indice-1])
+        slideAu(img[indice-1])
+
+        indice = indice -1
+        
+    }
+
+}
 
 const imgSlide = () => {
 
 
-        setInterval(() => {
-            slideDi(img[indice])
-            slideAu(img[indice+1])
+    slideDi(img[indice])
+    slideAu(img[indice+1])
 
-            indice = indice + 1
-            // slideDi(img[5])
-            if (indice === 5 ) {
-                indice = 1
-                // slideDi(img[5])
-                // slideAu(img[1])
-                slideDi(img[indice+1])
-                slideAu(img[indice-1])
+    indice = indice + 1
 
-                indice = indice -1
-
-
-            }
-                
             
-            
+    if (indice === 6 ) {
+        indice = 1
+        slideDi(img[indice])
+        slideAu(img[indice])
 
-        }, 1000);
+        indice = indice 
         
+    }
 
-    
 }
+let timers = setInterval(imgSlide, 2000);
+
+const imgSlideStop = () => {
+    clearInterval(timers)
+}
+
+const imgSlideStart = () => {
+     timers = setInterval(imgSlide, 2000);
+
+
+}
+
+
 
